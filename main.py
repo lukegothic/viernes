f295d66aa278222f46ec2cb5c27b55831ab796ef
Iván Pérez <lukegothic@gmail.com>
2017-11-16 17:01:37 +0100

    initial

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from enum import Enum
 class Hazard:
-    def __init__(self, name='', values=[0,0,0], reward=None):
+    def __init__(self, name='', values=[0,0,0], freeactions=0, reward=None):
         self.name = name
         self.values = values
+        self.freeactions = freeactions
         self.reward = reward
-class Boss:
+class Pirate:
     def __init__(self, name='', value=0, special=None):
         self.name = name
         self.value = value
@@ -12,14 +13,37 @@ class Boss:
 class Skill(Enum):
      LIFE = 1
      DRAW = 2
-     SCRY = 3
-     EXCHANGE = 4
-class CardSkill:
-    def __init__(self, skill=None, power=1):
+     DOUBLE = 3
+     DESTROY = 4
+     BELOW = 5
+     COPY = 6
+     EASY = 7
+     SORT = 8
+     EXCHANGE = 9
+     HIGHESTISZERO = 10
+     STOP = 11
+class ActionSkill:
+    def __init__(self, skill=None, value=1):
         self.skill = skill
-        self.power = power
-class Card:
-    def __init__(self, name='', value=0, skill=None):
+        self.value = value
+class Action:
+    def __init__(self, name='', value=0, skill=None, age=0):
         self.name = name
         self.value = value
         self.skill = None
+        self.age = age
+def start():
+    stage = 1
+    lives = 20
+    # deck inicio
+    actions = []
+    actions.append(Action("2", 2))
+    for i in range(2):
+        actions.append(Action("1", 1))
+    for i in range(8):
+        actions.append(Action("0", 0))
+    actions.append(Action("02LIFE", 1, ActionSkill(Skill.LIFE, 2)))
+    for i in range(5):
+        actions.append(Action("-1", -1))
+    print(vars(actions[0]))
+start()
